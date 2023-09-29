71328b568d6a8ee3d10953abbc5e152830f0e719
Alex Huang <whuang288@wisc.edu>
2023-09-28 20:24:25 -0500

    save predictions for faster inference

--- app.py
+++ app.py
@@ -7,67 +7,68 @@ import plotly.graph_objs as go
 from utils import MyLSTM
 
 
-# load the model
-in_window, out_window = 50, 50
-model_type = "Linear"
-if model_type == "LSTM":
-    model = MyLSTM(in_dim=1, hidden_size=128, out_size=out_window, num_layers=3)
-elif model_type == "Linear":
-    model = nn.Sequential(
-        nn.Linear(in_window, 32),
-        nn.ReLU(),
-        nn.Linear(32, 16),
-        nn.ReLU(),
-        nn.Linear(16, out_window)
-    )
-else:
-    raise ValueError("Invalid model type!")   
-model.load_state_dict(torch.load(f'./ckpt/{model_type}.pt'))
-model = model.double()
+## load the model
+# in_window, out_window = 50, 50
+# model_type = "Linear"
+# if model_type == "LSTM":
+#     model = MyLSTM(in_dim=1, hidden_size=128, out_size=out_window, num_layers=3)
+# elif model_type == "Linear":
+#     model = nn.Sequential(
+#         nn.Linear(in_window, 32),
+#         nn.ReLU(),
+#         nn.Linear(32, 16),
+#         nn.ReLU(),
+#         nn.Linear(16, out_window)
+#     )
+# else:
+#     raise ValueError("Invalid model type!")   
+# model.load_state_dict(torch.load(f'./ckpt/{model_type}.pt'))
+# model = model.double()
 
 
 
-# load the data
-original_data = np.load('data/time_series_data.npy')
-time_series_data = np.zeros(365*2, dtype=np.float64)
-time_series_data[:in_window] = original_data[:in_window]
+## load the data
+# original_data = np.load('data/time_series_data.npy')
+# time_series_data = np.zeros(365*2, dtype=np.float64)
+# time_series_data[:in_window] = original_data[:in_window]
 
 
 
-# run the inference
-model.eval()
-for index in range(in_window, 365*2, out_window):
-    if model_type == "LSTM":
-        data = torch.tensor(time_series_data[index-in_window: index]).to(torch.float64).view(1, -1, 1)
-    elif model_type == "Linear":
-        data = torch.tensor(time_series_data[index-in_window: index]).to(torch.float64).view(1, -1)
-    with torch.no_grad():
-        predicted = model(data)
-        if index + out_window > 365*2:
-            time_series_data[index:] = predicted[0].numpy()[:365*2-index]
-        else:
-            time_series_data[index : index + out_window] = predicted[0].numpy()
+## run the inference
+# model.eval()
+# for index in range(in_window, 365*2, out_window):
+#     if model_type == "LSTM":
+#         data = torch.tensor(time_series_data[index-in_window: index]).to(torch.float64).view(1, -1, 1)
+#     elif model_type == "Linear":
+#         data = torch.tensor(time_series_data[index-in_window: index]).to(torch.float64).view(1, -1)
+#     with torch.no_grad():
+#         predicted = model(data)
+#         if index + out_window > 365*2:
+#             time_series_data[index:] = predicted[0].numpy()[:365*2-index]
+#         else:
+#             time_series_data[index : index + out_window] = predicted[0].numpy()
 
 
-# denormalize the data
-original_mean = np.load("./data/mean.npy")
-original_std = np.load("./data/std.npy")
-time_series_data = time_series_data * original_std + original_mean
+# # denormalize the data
+# original_mean = np.load("./data/mean.npy")
+# original_std = np.load("./data/std.npy")
+# time_series_data = time_series_data * original_std + original_mean
 
 
-# transform daily data to monthly data
-month_day_count = np.load("./data/month_day_count.npy").tolist()
-month_day_count += month_day_count
-predictions = []
-total_count = 0
-for index in range(24):
-    predictions.append(sum(time_series_data[total_count: total_count + month_day_count[index]]))
-    total_count += month_day_count[index]
-predictions = np.array(predictions) / 1000000  
+## transform daily data to monthly data
+# month_day_count = np.load("./data/month_day_count.npy").tolist()
+# month_day_count += month_day_count
+# predictions = []
+# total_count = 0
+# for index in range(24):
+#     predictions.append(sum(time_series_data[total_count: total_count + month_day_count[index]]))
+#     total_count += month_day_count[index]
+# predictions = np.array(predictions) / 1000000 
+# np.save("./data/predictions.npy", predictions) 
 
 
 st.title("Receipt Count Predictor")
-
+predictions = np.load("./data/predictions.npy")
 
 # Input form for month, only allow 1-12 for 2022 January to 2022 December
 month = st.number_input("Enter month (1-12)", min_value=1, max_value=12, value=datetime.datetime.now().month)
